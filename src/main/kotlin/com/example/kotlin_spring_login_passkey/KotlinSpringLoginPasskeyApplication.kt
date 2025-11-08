e8b952f91c65ac100577ea2ae8324f0fdea38a29
Rae-An Andres <raeandres@users.noreply.github.com>
2025-11-08 16:33:39 +0800

    main init

---
+++ src/main/kotlin/com/example/kotlin_spring_login_passkey/KotlinSpringLoginPasskeyApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlin_spring_login_passkey
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringLoginPasskeyApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinSpringLoginPasskeyApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/kotlin_spring_login_passkey/KotlinSpringLoginPasskeyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlin_spring_login_passkey
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringLoginPasskeyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
